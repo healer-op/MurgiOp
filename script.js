09cc12d3075e7c89de9736f6ccccce3a4e5f98fe
HEALER <65026164+healer-op@users.noreply.github.com>
2021-06-20 13:17:18 +0530

    Update script.js

--- script.js
+++ script.js
@@ -442,19 +442,19 @@ document.getElementById('right').addEventListener("click", () => move('right'));
 
 window.addEventListener("keydown", event => {
   moveSound.play();
-  if (event.keyCode == '38') {
+  if (event.keyCode == '38' || event.keyCode == '87') {
     // up arrow
     move('forward');
   }
-  else if (event.keyCode == '40') {
+  else if (event.keyCode == '40' || event.keyCode == '83') {
     // down arrow
     move('backward');
   }
-  else if (event.keyCode == '37') {
+  else if (event.keyCode == '37' || event.keyCode == '65') {
     // left arrow
     move('left');
   }
-  else if (event.keyCode == '39') {
+  else if (event.keyCode == '39' || event.keyCode == '68') {
     // right arrow
     move('right');
   }
